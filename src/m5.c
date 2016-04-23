435ef86537c84cf5ac156b4509d8f89f9145ecdc
Seth Girvan <sethgirvan@gmail.com>
2016-04-23 15:12:16 -0700

    Update and fix CRCs
    I forgot to update the header CRC for the increased number of Motor IDs,
    and there was an error where the payload CRC was statically initialized
    rather than being reset for each packet.

--- src/m5.c
+++ src/m5.c
@@ -39,7 +39,7 @@
 
 // Manually calculated assuming NUM_THRUSTERS == 8. Update if any part of the
 // header changes! (32 bit CRC: ANSI X3.66 per VRCSR doc)
-#define PROPULSION_COMMAND_HEADER_CRC ((uint_fast32_t)0xDAE70014)
+#define PROPULSION_COMMAND_HEADER_CRC ((uint_fast32_t)0xDD8AC40D)
 
 // Manually calculated assuming R_ID == 0 (without final xor mask)
 #define CRC_POST_R_ID ((uint_fast32_t)0xEACEB963)
@@ -118,7 +118,8 @@ static bool pack_power(unsigned char *c)
 			*c = PROPULSION_COMMAND_R_ID;
 		}
 
-		static uint32_t crc = CRC_POST_R_ID;
+		static uint32_t crc;
+		crc = CRC_POST_R_ID;
 
 		{
 			// We delay committing to power values as long as possible, so we
