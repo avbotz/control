311546abc7f74447fc5993e68c0b2b6c3e1e4ee4
Seth Girvan <sethgirvan@gmail.com>
2016-04-30 19:34:35 -0700

    Improve logging of pc file io failures

--- src/pc/io_depth_pc.cpp
+++ src/pc/io_depth_pc.cpp
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <assert.h>
 
+#include "dbg.h"
+
 static FILE *depth_stream;
 
 void io_depth_init(char const *path)
 {
 	depth_stream = fopen("depth_in", "r");
+	if (!depth_stream)
+	{
+		DEBUG("Failed to open %s", path);
+	}
 	return;
 }
 
@@ -13,6 +19,10 @@ float io_depth()
 {
 	assert(depth_stream);
 	float val;
-	fscanf(depth_stream, "%f ", &val);
+	if(fscanf(depth_stream, "%f ", &val) != 1)
+	{
+		DEBUG("Failed reading depth");
+		return 0.f;
+	}
 	return val;
 }
